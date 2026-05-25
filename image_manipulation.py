d87c0d7adf40a68ce72761197dc9b0e73624194d
simmk15 <sknagra15@gmail.com>
2026-05-25 21:35:38 +0530

    Add files via upload

---
+++ image_manipulation.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image = cv2.imread('img.jpg')
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image_rgb)
+plt.title("Original Image")
+plt.show()
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray_image, cmap='gray')
+plt.title("Grayscale Image")
+plt.show()
+cropped_image = image[9:200, 40:200]
+cropped_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+plt.imshow(cropped_rgb)
+plt.title("Cropped Region")
+plt.show()
+(h, w) = image.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+rotated_rgb = cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgb)
+plt.title("Rotated Image")
+plt.show()
+brightness_matrix = np.ones(image.shape, dtype="uint8") * 70
+brighter = cv2.add(image, brightness_matrix)
+brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BGR2RGB)
+plt.imshow(brighter_rgb)
+plt.title("Brighter Image")
+plt.show()
+cv2.imwrite('img/grayscale_image.jpg', gray_image)
+cv2.imwrite('img/cropped_image.jpg', cropped_image)
+cv2.imwrite('img/rotated_image.jpg', rotated)
+cv2.imwrite('img/brighter_image.jpg', brighter)
