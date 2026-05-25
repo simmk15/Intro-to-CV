b9d0ccc3941c7f875d72256e1121c06161045f92
simmk15 <sknagra15@gmail.com>
2026-05-25 23:54:38 +0530

    Add files via upload

---
+++ color_filter_app.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+def apply_color_filter(image,filter_type):
+    filtered_image=image.copy()
+    if filter_type=="red_tint":
+        filtered_image[:,:,1]=0
+        filtered_image[:,:,0]=0
+    elif filter_type=="blue_tint":
+        filtered_image[:,:,1]=0
+        filtered_image[:,:,2]=0
+    elif filter_type=="green tint":
+        filtered_image[:,:,0]=0
+        filtered_image[:,:,2]=0
+    elif filter_type=="increased_red":
+        filtered_image[:,:,2]=cv2.add(filtered_image[:,:,2],50)
+    elif filter_type=="increased_green":
+        filtered_image[:,:,1]=cv2.add(filtered_image[:,:,2],50)
+    elif filter_type=="increased_blue":
+        filtered_image[:,:,0]=cv2.add(filtered_image[:,:,0],50)
+    elif filter_type=="decreased_blue":
+        filtered_image[:,:,0]=cv2.subtract(filtered_image[:,:,0],50)
+    elif filter_type=="decreased_red":
+        filtered_image[:,:,2]=cv2.subtract(filtered_image[:,:,2],50)
+    elif filter_type=="decreased_green":
+        filtered_image[:,:,1]=cv2.subtract(filtered_image[:,:,0],50)
+    return filtered_image
+image_path='img.jpg'
+image=cv2.imread(image_path)
+if image is None:
+    print("Error! Image not found!")
+else:
+    filter_type="original"
+    print("Press the following keys to apply filters:")
+    print("r- Red Tint")
+    print("b- Blue Tint")
+    print("g- Green Tint")
+    print("i- Increase Red Intensity")
+    print("p- Increased Blue Intensity")
+    print("v- Increased Green Intensity")
+    print("l- Decrease Red Intensity")
+    print("z- Decrease Green Intensity")
+    print("d- Decrease Blue Intensity")
+    print("q- Quit")
+    while True:
+        filtered_image=apply_color_filter(image,filter_type)
+        cv2.imshow("Filtered image",filtered_image)
+        key=cv2.waitKey(0)& 0xFF
+        if key==ord('r'):
+            filter_type="red_tint"
+        elif key==ord('b'):
+            filter_type="blue_tint"
+        elif key==ord('g'):
+            filter_type="green_tint"
+        elif key==ord('i'):
+            filter_type="increased_red"
+        elif key==ord('d'):
+            filter_type="decreased_blue"
+        elif key==ord('p'):
+            filter_type="increased_blue"
+        elif key==ord('v'):
+            filter_type="increased_green"
+        elif key==ord('l'):
+            filter_type="decreased_red"
+        elif key==ord('z'):
+            filter_type="decreased_green"
+        elif key==ord('q'):
+            print("Existing...")
+            break
+        else:
+            print("Invalid key! Please use 'r','b','g','i','d','p','v','l','z' or 'q'.")
+cv2.destroyAllWindows()
